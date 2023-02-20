0ac395362942579cb78d4bbaede3d0fff96866f0
Alexander Mandrikov <mandrikov@pm.me>
2023-02-20 09:05:19 +0700

    use random numbers in Timeout module

--- lib/kaize_votes/timeout.ex
+++ lib/kaize_votes/timeout.ex
@@ -3,22 +3,22 @@ defmodule KaizeVotes.Timeout do
 
   @spec before_vote() :: non_neg_integer()
   def before_vote do
-    :timer.seconds(5)
+    rand(:timer.seconds(5), :timer.seconds(10))
   end
 
   @spec before_next() :: non_neg_integer()
   def before_next do
-    :timer.seconds(2)
+    rand(:timer.seconds(3), :timer.seconds(7))
   end
 
   @spec before_login() :: non_neg_integer()
   def before_login do
-    :timer.seconds(3)
+    rand(:timer.seconds(10), :timer.seconds(15))
   end
 
   @spec wait_new_proposals() :: non_neg_integer()
   def wait_new_proposals do
-    :timer.minutes(5)
+    rand(:timer.minutes(15), :timer.minutes(25))
   end
 
   @doc """
