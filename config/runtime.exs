12f28a05ac0f2a078ae0d910acd438ed3629647a
Alexander Mandrikov <mandrikov@pm.me>
2023-02-22 16:43:42 +0700

    make fetch_env! raise only in :prod env

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,4 +1,6 @@
 import Config
 
-config :kaize_votes, :email, System.fetch_env!("KAIZE_EMAIL")
-config :kaize_votes, :password, System.fetch_env!("KAIZE_PASSWORD")
+if config_env() == :prod do
+  config :kaize_votes, :email, System.fetch_env!("KAIZE_EMAIL")
+  config :kaize_votes, :password, System.fetch_env!("KAIZE_PASSWORD")
+end
