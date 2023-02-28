3cd050c68af2fcf10548bcd32449488ed51ebca4
Alexander Mandrikov <mandrikov@pm.me>
2023-02-28 17:44:05 +0700

    add cookie_store.state:new/1

--- lib/kaize_votes/cookie_store/state.ex
+++ lib/kaize_votes/cookie_store/state.ex
@@ -16,4 +16,9 @@ defmodule KaizeVotes.CookieStore.State do
   def new(path, cookie) do
     %__MODULE__{path: path, cookie: cookie}
   end
+
+  @spec new(Path.t()) :: t()
+  def new(path) do
+    %__MODULE__{path: path}
+  end
 end
