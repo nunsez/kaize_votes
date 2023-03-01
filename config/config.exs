16f53d04cee17d1ee86c4636138f70c7c7fd1380
Alexander Mandrikov <mandrikov@pm.me>
2023-03-01 17:25:59 +0700

    fix config_env path

--- config/config.exs
+++ config/config.exs
@@ -6,8 +6,9 @@ config :logger, :console,
 config :kaize_votes, :http_client, KaizeVotes.Http
 
 
-config_env_file = "#{config_env()}.exs"
 
-if File.exists?(config_env_file) do
-  import_config(config_env_file)
+config_env_path = Path.expand("./#{config_env()}.exs", __DIR__)
+
+if File.exists?(config_env_path) do
+  import_config(config_env_path)
 end
