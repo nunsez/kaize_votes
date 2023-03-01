5d272f1f2b5c1a2e5f0e22fa663ff21bfdf13fcc
Alexander Mandrikov <mandrikov@pm.me>
2023-03-01 15:57:26 +0700

    fix import config error if config not exists

--- config/config.exs
+++ config/config.exs
@@ -6,4 +6,8 @@ config :logger, :console,
 config :kaize_votes, :http_client, KaizeVotes.Http
 
 
-import_config("#{config_env()}.exs")
+config_env_file = "#{config_env()}.exs"
+
+if File.exists?(config_env_file) do
+  import_config(config_env_file)
+end
