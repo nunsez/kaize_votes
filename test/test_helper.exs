e3cf17de7bda46da7020ac738e37af081f20c710
Alexander Mandrikov <mandrikov@pm.me>
2023-03-01 18:09:35 +0700

    add test_helper module

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,25 @@
 ExUnit.start()
+
+defmodule KaizeVotes.TestHelper do
+  @moduledoc false
+
+  alias KaizeVotes.Html
+
+  @spec fixture(Path.t()) :: String.t()
+  defmacro fixture(path) do
+    quote do
+      unquote(path)
+      |> Path.expand(__DIR__)
+      |> File.read!()
+    end
+  end
+
+  @spec doc(Path.t()) :: Html.document()
+  defmacro doc(file) do
+    quote do
+      unquote(file)
+      |> fixture()
+      |> Html.parse()
+    end
+  end
+end
